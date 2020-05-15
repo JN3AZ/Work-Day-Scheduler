685e9dace9392cac70d572f186620d4ef9149eb9
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-14 23:32:30 -0700

    added bold font to the hours in css

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const date = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(date);
 
-$(".time-block").each(function (){
+$(".time-block").each(function(){
     const currentHour = parseInt(moment().format("H"));
     const hour = parseInt($(this).attr("data-hour"));
 
-    if(hour < currentHour) {
+    if(hour < currentHour){
         //past time block
         $(this).find("textarea").addClass("past");
-    } else if (hour > currentHour) {
+    }else if (hour > currentHour){
         //future
         $(this).find("textarea").addClass("future");
-    } else {
+    }else {
         //curent hour
         $(this).find("textarea").addClass("present");
     }
