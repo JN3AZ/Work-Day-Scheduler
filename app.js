b32f6ad55e57e4ac6524f7147314ef4de0c7a3ac
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-20 18:31:30 -0700

    added time block attr to each div class to allow past/present/future color code to work also fixed bootstrap functunality to work on small screens properly by col-3 col-7 and col-2 to respective areas

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ $("#currentDay").text(date);
 $(".time-block").each(function(){
     const currentHour = parseInt(moment().format("H"));
     const hour = parseInt($(this).attr("data-hour"));
-
+    
+    
+    
     if(hour < currentHour){
+        
         //past time block
         $(this).find("textarea").addClass("past");
     }else if (hour > currentHour){
+        
         //future
         $(this).find("textarea").addClass("future");
     }else {
+        
         //curent hour
         $(this).find("textarea").addClass("present");
     }
-    // console.log(hour, currentHour);
 });
 
 $("#9a").val(localStorage.getItem("9a"));
